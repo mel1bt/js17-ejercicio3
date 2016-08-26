e080e87ba95ad097ff49fe8b3205c0ac82e72e59
mel1bt <mberrocal.tipte@gmail.com>
2016-08-25 23:36:51 -0500

    Corrigiendo

--- js/app3.js
+++ js/app3.js
@@ -1,15 +1,16 @@
 var numero = parseInt(prompt("Ingrese numero"));
-if ( numero % 2 == 0) {
-	if (numero > 2 && numero < 5) {
-		document.write("Good!");
-	} else	if (numero > 6 && numero < 20) {
-		document.write("Great!");
+if ( numero > 1 && numero < 100 ) {
+	if ( numero % 2 == 0) {
+		if (numero > 2 && numero < 5) {
+			document.write("Good!");
+		} else	if (numero > 6 && numero < 20) {
+			document.write("Great!");
+		}
+		else if (numero>20) {
+			document.write("Perfect!");
+		}
+	} 
+	else {
+		document.write("Odd!");
 	}
-	else if (numero>20) {
-		document.write("Perfect!");
-	}
-} 
-else {
-	document.write("Odd!");
 }
-
